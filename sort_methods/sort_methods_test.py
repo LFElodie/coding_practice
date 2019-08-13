cfa738236ed1c48a29ebdc19bcb207325084b0ce
lanfei <lanfei@momenta.ai>
2019-08-13 13:09:02 +0800

    add counting sort to methods test

--- sort_methods/sort_methods_test.py
+++ sort_methods/sort_methods_test.py
@@ -7,14 +7,16 @@ from sort_methods.insert_sort import insert_sort
 from sort_methods.merge_sort import merge_sort
 from sort_methods.quick_sort import quick_sort
 from sort_methods.heap_sort import heap_sort
+from sort_methods.counting_sort import counting_sort
 import sys
-# sys.setrecursionlimit(100000)
+sys.setrecursionlimit(100000)
 
 if __name__ == '__main__':
-    raw_list = list(np.random.rand(10000))
+    raw_list = list(np.random.randint(low=0, high=10000, size=10000))
     print(len(raw_list))
     bubble_sort(raw_list)
     insert_sort(raw_list)
     merge_sort(raw_list)
     quick_sort(raw_list)
     heap_sort(raw_list)
+    counting_sort(raw_list)
