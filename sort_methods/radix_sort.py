52da6c1862c81c3d7e2a8848f0079a048f295881
lanfei <lanfei@momenta.ai>
2019-08-14 11:43:27 +0800

    add radix sort

---
+++ sort_methods/radix_sort.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from utils import time_it, assert_sort
+
+
+@assert_sort
+@time_it
+def radix_sort(sort_list: list) -> list:
+    def counting_sort(sort_list: list, exp: int) -> list:
+        k = 10
+        counting_list = [0] * k
+        result = [0] * len(sort_list)
+        for num in sort_list:
+            counting_list[(num // exp) % 10] += 1
+        for i in range(1, k):
+            counting_list[i] += counting_list[i - 1]
+        for j in range(len(sort_list) - 1, -1, -1):
+            result[counting_list[(sort_list[j] // exp) % 10] - 1] = sort_list[j]
+            counting_list[(sort_list[j] // exp) % 10] -= 1
+        return result
+    max_num = max(sort_list)
+    exp = 1
+    while max_num/exp > 0:
+        sort_list = counting_sort(sort_list, exp)
+        exp *= 10
+    return sort_list
+
+
+if __name__ == '__main__':
+    raw_list = [15, 489, 45, 4, 28, 16, 73, 45, 85, 96, 26, 87, 26, 51, 32] * 100
+    print(radix_sort(raw_list))
--- sort_methods/sort_methods_test.py
+++ sort_methods/sort_methods_test.py
@@ -8,11 +8,12 @@ from sort_methods.merge_sort import merge_sort
 from sort_methods.quick_sort import quick_sort
 from sort_methods.heap_sort import heap_sort
 from sort_methods.counting_sort import counting_sort
+from sort_methods.radix_sort import radix_sort
 import sys
 sys.setrecursionlimit(100000)
 
 if __name__ == '__main__':
-    raw_list = list(np.random.randint(low=0, high=10000, size=10000))
+    raw_list = [int(num) for num in np.random.randint(low=0, high=10000, size=10000)]
     print(len(raw_list))
     bubble_sort(raw_list)
     insert_sort(raw_list)
@@ -20,3 +21,4 @@ if __name__ == '__main__':
     quick_sort(raw_list)
     heap_sort(raw_list)
     counting_sort(raw_list)
+    radix_sort(raw_list)
